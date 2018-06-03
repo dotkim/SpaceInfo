2e7d9ece7f35af2cdae97603c49e31821478e7f1
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2018-06-03 03:04:32 +0200

    DB Install

--- SpaceInfo.py
+++ SpaceInfo.py
@@ -1,8 +1,17 @@
-import wmi, pathlib
+import wmi, pathlib, sqlite3
 pathlib.Path('.\\Logs').mkdir(parents=True, exist_ok=True)
+database = '.\\Logs\DiskLogs.db'
+
+def dbConnection(database):
+    try:
+        conn = sqlite3.connect(database)
+        return conn
+    except sqlite3.Error as err:
+        self.log.error('dberror: %s' % err)
 
 def getServersFromFile():
     try:
+        dbConnection(database)
         serverNameFromFile = open('.\Servers.txt', 'r')
         serverNames = serverNameFromFile.read().splitlines()
         return serverNames
@@ -11,17 +20,20 @@ def getServersFromFile():
 
 def printDiskUsageToFile():
     serverNames = getServersFromFile()
-    printToFile = open('.\Logs\DiskUsage.txt', 'a')
+    conn = dbConnection(database)
+    cur = conn.cursor()
+    #printToFile = open('.\Logs\DiskUsage.txt', 'a')
     for server in serverNames:
         c = wmi.WMI(server)
         for disk in c.Win32_LogicalDisk(DriveType=3):
-            printToFile.write(server + ' ' + disk.Caption + "%0.2f%% free" % (100.0 * int(disk.FreeSpace) / int(disk.Size)) + '\n') 
-    printToFile.close()
+            cur.execute("""
+                INSERT INTO DiskUsage(ServerID, Caption, FreeSpace, Size, DateCaptured)
+                VALUES ('{SER}', '{CAP}', {FRS}, {SIZ}, DATE('NOW'))
+            """\
+            .format(SER=server, CAP= disk.caption, FRS=int(disk.FreeSpace), SIZ=int(disk.Size)))
+            #printToFile.write(server + ' ' + disk.Caption + "%0.2f%% free" % (100.0 * int(disk.FreeSpace) / int(disk.Size)) + '\n') 
+    #printToFile.close()
+    conn.commit()
+    conn.close()
 
 printDiskUsageToFile()
-
-def mainApp():
-    # Sjekke servere for diskplass %
-    # Lage rapport for diskplass
-    # Evt. slette gamle rapporter?
-    # Varsle ved lav %
